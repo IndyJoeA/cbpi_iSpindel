400d192e27738f257e06ed8ec50f13b63387b77e
IndyJoeA <IndyJoeA@users.noreply.github.com>
2017-06-20 22:54:01 -0400

    Data types added

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 import os
 from subprocess import Popen, PIPE, call
 
-from modules import cbpi
-from modules.core.hardware import ActorBase, SensorPassive, SensorActive
+from modules import cbpi, socketio
+from modules.core.hardware import SensorActive
 import json
 from flask import Blueprint, render_template, jsonify, request
 from modules.core.props import Property
@@ -10,10 +11,32 @@ from modules.core.props import Property
 blueprint = Blueprint('hydrometer', __name__)
 cache = {}
 
+def calcGravity(polynom, tilt):
+	# Calculate gravity from polynomial
+	tilt = float(tilt)
+	result = eval(polynom)
+	result = round(float(result),2)
+	return result
+
 @cbpi.sensor
 class Hydrometer(SensorActive):
 	key = Property.Text(label="Sensor ID", configurable=True)
-	sensorType = Property.Select("Data Type", options=["Temperature", "Angle", "Battery"])
+	sensorType = Property.Select("Data Type", options=["Temperature", "Gravity", "Battery"])
+	tuningPolynom = Property.Text(label="Tuning Polynomial", configurable=True, default_value="tilt")
+	unitsGravity = Property.Select("Gravity Units", options=["SG", "Brix", "°P"])
+
+	def get_unit(self):
+		if self.sensorType == "Temperature":
+			return "°C" if self.get_config_parameter("unit", "C") == "C" else "°F"
+		elif self.sensorType == "Gravity":
+			return self.unitsGravity
+		elif self.sensorType == "Battery":
+			return "%"
+		else:
+			return " "
+
+	def stop(self):
+		pass
 
 	def execute(self):
 		global cache
@@ -35,8 +58,16 @@ def set_temp():
 	temp = data["temperature"]
 	angle = data["angle"]
 	battery = data["battery"]
-		
-	cache[id] = temp
+
+	for idx, value in cbpi.cache["sensors"].iteritems():
+		if type(value.instance) is Hydrometer:
+			if value.instance.sensorType == "Temperature":
+				cache[id] = temp
+			elif value.instance.sensorType == "Gravity":
+				cache[id] = calcGravity(value.instance.tuningPolynom, angle)
+			elif value.instance.sensorType == "Battery":
+				cache[id] = battery
+
 	return ('', 204)
 
 @cbpi.initalizer()
